bb9d3acda5f97638e866845688a984d92995049d
Jerome Onwunalu <jeronwunalu@gmail.com>
2021-10-28 14:42:52 -0500

    Allow returning and printing of greeting

--- src/hello-world.lisp
+++ src/hello-world.lisp
@@ -5,6 +5,8 @@
 (in-package :hello-world/src/hello-world)
 
 (defun greet-user (&optional (name "John"))
-  (let* ((name (or (nth 1 sb-ext:*posix-argv*) name)))
-    (format t "~&Hello ~a!" (string-capitalize name))))
+  (let* ((name (or (nth 1 sb-ext:*posix-argv*) name))
+         (greeting (format nil "Hello ~a!" (string-capitalize name))))
+    (format t "~&~a" greeting)
+    greeting))
 

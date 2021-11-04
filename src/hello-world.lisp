704a4c222fda48d8df920b9936a37c01b299a4bc
Jerome Onwunalu <jeronwunalu@gmail.com>
2021-11-03 21:56:31 -0500

    Remove statement printing to standard output

--- src/hello-world.lisp
+++ src/hello-world.lisp
@@ -6,6 +6,5 @@
 (defun greet-user (&optional (name "John"))
   (let* ((name (or (nth 1 sb-ext:*posix-argv*) name))
          (greeting (format nil "Hello ~a!" (string-capitalize name))))
-    (format t "~&~a" greeting)
     greeting))
 

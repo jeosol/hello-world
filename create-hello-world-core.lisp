3e1fe21c9e52a8ae5d1081d5a837b1a45037d2cf
Jerome Onwunalu <jeronwunalu@gmail.com>
2021-11-03 22:09:38 -0500

    Load dependencies with quicklisp

--- create-hello-world-core.lisp
+++ create-hello-world-core.lisp
@@ -1,4 +1,4 @@
-;(pushnew (truename "/root/src/projects/") ql:*local-project-directories*)
-;(ql:register-local-projects)
+(ql:quickload "fiveam")
+(ql:quickload "fiveam-asdf")
 (ql:quickload "hello-world")
 (sb-ext:save-lisp-and-die "hello-world.core" :executable nil)

fdd24b69cdf1541154a788e69ef67272da3b82cc
Jerome Onwunalu <jeronwunalu@gmail.com>
2021-11-06 23:09:20 -0500

    Add function doc string

--- src/hello-world.lisp
+++ src/hello-world.lisp
@@ -4,6 +4,7 @@
 (in-package :hello-world/src/hello-world)
 
 (defun greet-user (&optional (name "John"))
+  "Print a greeting for a user."
   (let* ((name (or (nth 1 sb-ext:*posix-argv*) name))
          (greeting (format nil "Hello ~a!" (string-capitalize name))))
     greeting))

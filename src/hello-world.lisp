f1c74b38e5d47e8aeb935b5f9e01bb97dfc71c77
Jerome Onwunalu <jeronwunalu@gmail.com>
2021-11-09 15:47:15 -0600

    print greeting before exiting; also returns the greeting

--- src/hello-world.lisp
+++ src/hello-world.lisp
@@ -8,5 +8,5 @@
   "Print a greeting for a user."
   (let* ((name (or (nth 1 sb-ext:*posix-argv*) name))
          (greeting (format nil "Hello ~a!" (string-capitalize name))))
-    greeting))
+    (print greeting)))
 
